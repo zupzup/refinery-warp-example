fe31037e65b14546d07104ff913c95b0e7d4556d
Mario Zupan <mario@zupzup.org>
2020-05-13 22:31:13 +0200

    apply

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,10 @@ mod embedded {
 
 #[derive(Debug)]
 struct MigrationRow {
-    version: String,
+    version: i32,
     name: String,
     applied_on: String,
+    checksum: String,
 }
 
 #[tokio::main]
@@ -47,8 +48,9 @@ async fn run_migrations() -> std::result::Result<(), Error> {
         .run_async(&mut client)
         .await?;
     let migration_state_after = fetch_migration_state(&client).await?;
+
     if migration_state_after.len() > migration_state_before.len() {
-        let deployed_versions: Vec<String> = migration_state_before
+        let deployed_versions: Vec<i32> = migration_state_before
             .into_iter()
             .map(|m| m.version)
             .collect();
@@ -56,9 +58,12 @@ async fn run_migrations() -> std::result::Result<(), Error> {
             .into_iter()
             .filter(|m| !deployed_versions.contains(&m.version))
             .collect();
+
         for m in delta.iter() {
-            println!("migrations applied: {:?}", m);
+            println!("Migration applied: {:?}", m);
         }
+    } else {
+        println!("No migrations to apply");
     }
 
     println!("DB migrations finished!");
@@ -69,20 +74,29 @@ async fn run_migrations() -> std::result::Result<(), Error> {
 async fn fetch_migration_state(
     client: &Client,
 ) -> std::result::Result<Vec<MigrationRow>, tokio_postgres::Error> {
-    let rows = client
+    let rows = match client
         .query("SELECT * from refinery_schema_history", &[])
-        .await?;
+        .await
+    {
+        Ok(v) => v,
+        Err(e) => {
+            eprintln!("could not fetch migration state - first run? {}", e);
+            return Ok(vec![]);
+        }
+    };
 
     Ok(rows.iter().map(|r| to_migration_row(&r)).collect())
 }
 
 fn to_migration_row(row: &Row) -> MigrationRow {
-    let version: String = row.get(0);
-    let name: String = row.get(0);
-    let applied_on: String = row.get(0);
+    let version: i32 = row.get(0);
+    let name: String = row.get(1);
+    let applied_on: String = row.get(2);
+    let checksum: String = row.get(3);
     MigrationRow {
         version,
         name,
         applied_on,
+        checksum,
     }
 }
